5108e6ba9f177bbb8d65ef45fa11a078d48da9e5
gabriel <banegasgabriel@gmail.com>
2020-01-11 12:39:05 -0300

    se agrego operacion matematicas

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 console.log('hola me falto este mensaje');
+// vamos a hacer una suma y devolverla en el console
+var a = 10 ;
+var b = 20 ;
+
+var suma = a + b;
+var resta = a - b;
+var multiplicacion = a * b;
+
+console.log("esta es la suma " + suma);
+console.log("esta es la resta " + resta);
+console.log("esta es la multiplicacion " + multiplicacion);
